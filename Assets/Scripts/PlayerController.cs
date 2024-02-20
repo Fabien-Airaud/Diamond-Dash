3d0df4b416e356a5531fdb52c54f6652bc9f3827
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-20 18:04:02 -0500

    Get rigidbody at start, add gravity and add force in horizontal and vertical axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        playerRb.AddForce(verticalInput * speed * Vector3.forward, ForceMode.Impulse);
+        playerRb.AddForce(horizontalInput * speed * Vector3.right, ForceMode.Impulse);
     }
 }
