d9b92e71fab2381c680d8e910c919d5a1f73535f
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-20 18:25:56 -0500

    Add player xBounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 10.0f;
     private Rigidbody playerRb;
+
+    private float speed = 10.0f;
+    private float xBound = 7.0f;
 
 
     // Start is called before the first frame update
@@ -18,5 +20,16 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         playerRb.AddForce(horizontalInput * speed * Vector3.right, ForceMode.Impulse);
+
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+            playerRb.velocity = Vector3.zero;
+        }
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+            playerRb.velocity = Vector3.zero;
+        }
     }
 }
