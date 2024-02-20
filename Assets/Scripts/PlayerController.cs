2a50872dde14453ea44775fde13cb71ddae6a2a9
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-20 18:32:28 -0500

    Can jump only when on ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 
     private float speed = 10.0f;
     private float jumpForce = 10.0f;
+
     private float xBound = 7.0f;
+    private bool isGrounded = true;
 
 
     // Start is called before the first frame update
@@ -33,9 +35,19 @@ public class PlayerController : MonoBehaviour
             playerRb.velocity = Vector3.zero;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRb.AddForce(jumpForce * playerRb.mass * Vector3.up, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 }
