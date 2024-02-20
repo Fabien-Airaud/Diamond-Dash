edd12d3bc7d0cc20e3e1b3a13a652ceadd7fe887
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-20 18:46:28 -0500

    Clean up code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRb;
-
     private float speed = 10.0f;
     private float jumpForce = 10.0f;
 
     private float xBound = 7.0f;
     private bool isGrounded = true;
+
+    private Rigidbody playerRb;
 
 
     // Start is called before the first frame update
@@ -21,10 +21,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+
+    // Move the player left and right and can jump
+    private void MovePlayer()
+    {
+        // Horizontal movement
         float horizontalInput = Input.GetAxis("Horizontal");
         if (isGrounded) playerRb.AddForce(horizontalInput * speed * Vector3.right, ForceMode.Impulse);
         else playerRb.AddForce(0.3f * horizontalInput * speed * Vector3.right, ForceMode.Impulse);
 
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRb.AddForce(jumpForce * playerRb.mass * Vector3.up, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    // Constrain the player position to the screen
+    private void ConstrainPlayerPosition()
+    {
         if (transform.position.x < -xBound)
         {
             transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
@@ -35,14 +55,7 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
             playerRb.velocity = Vector3.zero;
         }
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            playerRb.AddForce(jumpForce * playerRb.mass * Vector3.up, ForceMode.Impulse);
-            isGrounded = false;
-        }
     }
-
 
     private void OnCollisionEnter(Collision collision)
     {
