b18cc606b727495146fa22da75595458752ed853
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-27 15:52:41 -0500

    Destroy objects when out of bounds

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class MoveDown : MonoBehaviour
 {
-    private float speed = 5.0f;
+    private readonly float speed = 5.0f;
+    private readonly float zBound = -10.0f;
 
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speed * Time.deltaTime * Vector3.back);
+
+        if (transform.position.z < zBound) Destroy(gameObject);
     }
 }
